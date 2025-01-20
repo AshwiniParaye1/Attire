d4362aec8b8b62795f73b39a84d25e9856f4a925
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2025-01-20 10:14:28 +0530

    created custom fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,15 +4,20 @@ export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
-        foreground: "var(--foreground)",
+        foreground: "var(--foreground)"
       },
-    },
+      fontFamily: {
+        light: ["customLight"],
+        regular: ["customRegular"],
+        bold: ["customBold"]
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 } satisfies Config;
