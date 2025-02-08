27dc39b3647919bd7b53f8f907f6d4963a6712ea
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2025-02-08 19:58:28 +0530

    removed category name

--- components/CategoryPage.tsx
+++ components/CategoryPage.tsx
@@ -3,18 +3,9 @@
 import React from "react";
 import styles from "../app/CategoryPage.module.css"; // Import CSS Module
 
-const CategoryPage = ({
-  category,
-  images
-}: {
-  category: string;
-  images: string[];
-}) => {
+const CategoryPage = ({ images }: { category: string; images: string[] }) => {
   return (
     <div className={styles.container}>
-      <h1 className="text-center text-2xl font-bold my-4">
-        {category.toUpperCase()}
-      </h1>
       <div className={styles.imageScroller}>
         {images.map((image, index) => (
           <div
